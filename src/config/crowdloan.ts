db65532a4c8dd750b53ccf9402ae704bddcdcb42
Will Park <korea651@hanmail.net>
2021-11-05 20:58:34 +0900

    hotfix

--- src/config/crowdloan.ts
+++ src/config/crowdloan.ts
@@ -1,4 +1,4 @@
 export const PARA_ID = 2000;
 export const MINIMUM_STAKING_AMOUNT = 5;
-export const DEFAULT_REWARD_AMOUNT = 125;
-export const REWARD_RATIO = 25;
+export const DEFAULT_REWARD_AMOUNT = 150;
+export const REWARD_RATIO = 30;
