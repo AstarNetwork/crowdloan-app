25025f60cdf35f2b485c1ddeec4fccb9da225e86
Will Park <korea651@hanmail.net>
2021-11-07 11:20:35 +0900

    hotfix

--- src/pages/Crowdloan.vue
+++ src/pages/Crowdloan.vue
@@ -36,7 +36,7 @@ export default defineComponent({
   },
   setup() {
     const startTime = moment.utc([2021, 1, 1]);
-    const endTime = moment.utc([2021, 10, 11]);
+    const endTime = moment.utc([2021, 10, 18]);
     const statuses = ref<StatusData[]>(statusItems);
     const rules = ref<RuleData[]>(ruleItems);
     const faqs = ref<FaqData[]>(faqItems);
