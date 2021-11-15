536fa9cd87907419707adb301c0266704783828b
impelcrypto <impelcrypto@gmail.com>
2021-11-15 20:49:45 +0800

    fix: remove HubSpot from /external-header

---
+++ src/components/HubSpot.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="hubspotForm" v-once />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  mounted() {
+    const script = document.createElement('script');
+    script.src = 'https://js.hs-scripts.com/20284937.js';
+    document.body.appendChild(script);
+  }
+});
+</script>
