63e182b8df0034cf2effccc1635dfce3a272239e
Hoon Kim <mail@hoonkim.me>
2021-12-02 15:51:21 +0100

    disable contribution (fix patch)

--- src/components/Stake.vue
+++ src/components/Stake.vue
@@ -9,6 +9,7 @@
               src="@/assets/img/info.svg"
               class="w-4 h-4 align-middle inline"
             />
+            <!--
             Note: Proxy accounts and multi signatures are not able to receive
             rewards. Also, the hardware wallet is not supported now! <br />
             The minimum contribution is set to 5 DOT. <br />
@@ -19,6 +20,8 @@
               class="underline text-primary"
               >this article</a
             >.
+            -->
+            Note: The Parachain auction Crowdloan has ended! Contributions after the auction period will not receive any rewards.
           </p>
           <div class="form-container p-12">
             <!-- <div class="bg-orange text-white mb-4 py-2 text-xs font-bold">
@@ -142,27 +145,27 @@ import {
   MIN_BALANCE,
   PARA_ID,
   REWARD_RATIO,
-  UNIT
-} from '@/config/crowdloan';
-import { isValidAddressPolkadotAddress } from '@/config/polkadot';
-import { ActionTypes } from '@/store/action-types';
-import { ApiPromise } from '@polkadot/api';
-import { web3FromSource } from '@polkadot/extension-dapp';
-import { AccountInfo } from '@polkadot/types/interfaces';
-import { keyring } from '@polkadot/ui-keyring';
-import BN from 'bn.js';
-import { computed, defineComponent, inject, reactive, ref, watch } from 'vue';
-import { useStore } from 'vuex';
-import { StakeFormData } from '../data/StakeFormData';
-import Disclaimer from './Disclaimer.vue';
-import ModalAccount from './ModalAccount.vue';
-import AddressSmall from './shared/AddressSmall.vue';
-import Balance from './shared/Balance.vue';
-import Button from './shared/Button.vue';
-import Input from './shared/Input.vue';
-import InputAmount from './shared/InputAmount.vue';
-import InputWithCopy from './shared/InputWithCopy.vue';
-import Title from './shared/Title.vue';
+  UNIT,
+} from "@/config/crowdloan";
+import { isValidAddressPolkadotAddress } from "@/config/polkadot";
+import { ActionTypes } from "@/store/action-types";
+import { ApiPromise } from "@polkadot/api";
+import { web3FromSource } from "@polkadot/extension-dapp";
+import { AccountInfo } from "@polkadot/types/interfaces";
+import { keyring } from "@polkadot/ui-keyring";
+import BN from "bn.js";
+import { computed, defineComponent, inject, reactive, ref, watch } from "vue";
+import { useStore } from "vuex";
+import { StakeFormData } from "../data/StakeFormData";
+import Disclaimer from "./Disclaimer.vue";
+import ModalAccount from "./ModalAccount.vue";
+import AddressSmall from "./shared/AddressSmall.vue";
+import Balance from "./shared/Balance.vue";
+import Button from "./shared/Button.vue";
+import Input from "./shared/Input.vue";
+import InputAmount from "./shared/InputAmount.vue";
+import InputWithCopy from "./shared/InputWithCopy.vue";
+import Title from "./shared/Title.vue";
 
 export default defineComponent({
   components: {
@@ -174,27 +177,27 @@ export default defineComponent({
     Balance,
     AddressSmall,
     ModalAccount,
-    Disclaimer
+    Disclaimer,
   },
   setup(props, { emit }) {
     const store = useStore();
     const data = reactive<StakeFormData>(new StakeFormData());
-    const api: any = inject('api');
+    const api: any = inject("api");
 
     const modalAccount = ref(false);
     const modalDisclaimer = ref(false);
     const allAccounts = ref();
     const allAccountNames = ref();
-    const resultHash = ref('');
-    const referLink = ref('');
+    const resultHash = ref("");
+    const referLink = ref("");
 
     const burnsWarning =
       "Account with balance below the existential deposit will be reaped (Polkadot's existential deposit is 1 DOT)";
 
     // check referral address as querystring
     let params = new URL(window.location.href).searchParams;
-    let referral = params.get('referral');
-    console.log('ref', referral);
+    let referral = params.get("referral");
+    console.log("ref", referral);
     if (referral) {
       data.referralAddress = referral;
     }
@@ -205,7 +208,7 @@ export default defineComponent({
         // console.log('accounts', Object.keys(accounts));
         allAccounts.value = Object.keys(accounts);
         allAccountNames.value = Object.values(accounts).map((obj: any) =>
-          obj.option.name.replace('\n              ', '')
+          obj.option.name.replace("\n              ", "")
         );
 
         data.polkadotAddress = Object.keys(accounts)[0];
@@ -218,7 +221,7 @@ export default defineComponent({
         const account: AccountInfo = (await apiData.query.system.account(
           data.polkadotAddress
         )) as AccountInfo;
-        console.log('acc', account);
+        console.log("acc", account);
         data.availableAmount = account.data.free.toBn() || new BN(0);
       } catch (e) {
         console.error(e);
@@ -254,14 +257,14 @@ export default defineComponent({
 
     const validatePolkadotAddress = (value: string): boolean => {
       if (!value) {
-        data.errors['polkadotAddress'] = 'Polkadot address is required.';
+        data.errors["polkadotAddress"] = "Polkadot address is required.";
         return false;
       }
       const isAddressValid = isValidAddressPolkadotAddress(value);
       if (isAddressValid) {
-        data.errors['polkadotAddress'] = '';
+        data.errors["polkadotAddress"] = "";
       } else {
-        data.errors['polkadotAddress'] = 'Invalid Polkadot address.';
+        data.errors["polkadotAddress"] = "Invalid Polkadot address.";
       }
       return isAddressValid;
     };
@@ -272,7 +275,7 @@ export default defineComponent({
     ): boolean => {
       if (stakingAmount < MINIMUM_STAKING_AMOUNT) {
         data.errors[
-          'stakingAmount'
+          "stakingAmount"
         ] = `Staking amount should be greater than ${MINIMUM_STAKING_AMOUNT}.`;
         return false;
       }
@@ -287,8 +290,8 @@ export default defineComponent({
       const bnStakingAmount = new BN(data.stakingAmount * 10 ** UNIT);
 
       if (bnStakingAmount.gte(availableAmount)) {
-        data.errors['stakingAmount'] =
-          'Staking amount can not be greater than available amount.';
+        data.errors["stakingAmount"] =
+          "Staking amount can not be greater than available amount.";
         return false;
       }
 
@@ -296,34 +299,37 @@ export default defineComponent({
       const remainingBal = balance - stakingAmount;
       // Ref: https://wiki.polkadot.network/docs/build-protocol-info#existential-deposit
       if (MIN_BALANCE > remainingBal) {
-        data.errors['stakingAmount'] = burnsWarning;
+        data.errors["stakingAmount"] = burnsWarning;
         return true;
       }
 
-      data.errors['stakingAmount'] = '';
+      data.errors["stakingAmount"] = "";
       return true;
     };
 
     const initialize = async () => {
       data.stakingAmount = MINIMUM_STAKING_AMOUNT;
       data.estimatedAmount = DEFAULT_REWARD_AMOUNT;
-      data.referralAddress = '';
+      data.referralAddress = "";
       modalDisclaimer.value = false;
 
       await setAvailableAmount();
     };
 
-    const isEnableStaking = computed(
-      () =>
-        data.polkadotAddress &&
-        data.polkadotAddress.length > 0 &&
-        data.stakingAmount >= MINIMUM_STAKING_AMOUNT &&
-        data.availableAmount.div(new BN(10 ** UNIT)).toNumber() >=
-          MINIMUM_STAKING_AMOUNT &&
-        data.errors['polkadotAddress'] === '' &&
-        (data.errors['stakingAmount'] === '' ||
-          data.errors['stakingAmount'] === burnsWarning)
-    );
+    // const isEnableStaking = computed(
+    //   () =>
+    //     data.polkadotAddress &&
+    //     data.polkadotAddress.length > 0 &&
+    //     data.stakingAmount >= MINIMUM_STAKING_AMOUNT &&
+    //     data.availableAmount.div(new BN(10 ** UNIT)).toNumber() >=
+    //       MINIMUM_STAKING_AMOUNT &&
+    //     data.errors['polkadotAddress'] === '' &&
+    //     (data.errors['stakingAmount'] === '' ||
+    //       data.errors['stakingAmount'] === burnsWarning)
+    // );
+
+    // QUICK_FIX: the crowdloan period has been over
+    const isEnableStaking = computed(() => false);
 
     const onShowModalDisclaimer = (e: any) => {
       e.preventDefault();
@@ -336,7 +342,7 @@ export default defineComponent({
       store.dispatch(ActionTypes.SET_LOADING, { loading: true });
 
       const apiData: ApiPromise = (await api).api;
-      const injector = await web3FromSource('polkadot-js');
+      const injector = await web3FromSource("polkadot-js");
 
       const contributeTransaction = apiData.tx.crowdloan.contribute(
         PARA_ID,
@@ -346,12 +352,12 @@ export default defineComponent({
 
       if (data.referralAddress) {
         try {
-          const memo = apiData.createType('AccountId', data.referralAddress);
+          const memo = apiData.createType("AccountId", data.referralAddress);
           const memoTransaction = apiData.tx.crowdloan.addMemo(PARA_ID, memo);
 
           const batch = apiData.tx.utility.batchAll([
             contributeTransaction,
-            memoTransaction
+            memoTransaction,
           ]);
 
           await batch.signAndSend(
@@ -366,7 +372,7 @@ export default defineComponent({
                 console.error(`error occurred: ${error}`);
                 store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
                   msg: `error occurred: ${error}`,
-                  alertType: 'error'
+                  alertType: "error",
                 });
                 store.dispatch(ActionTypes.SET_LOADING, { loading: false });
                 return;
@@ -374,10 +380,10 @@ export default defineComponent({
 
               if (status.status.isFinalized) {
                 const hashResult = batch.hash.toHex();
-                console.log('hashResult', hashResult);
+                console.log("hashResult", hashResult);
                 store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
                   msg: `Thank you for your contribution!...!`,
-                  alertType: 'success'
+                  alertType: "success",
                 });
                 resultHash.value = hashResult;
                 store.dispatch(ActionTypes.SET_LOADING, { loading: false });
@@ -391,7 +397,7 @@ export default defineComponent({
           console.error(err);
           store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
             msg: `error occurred: ${err}`,
-            alertType: 'error'
+            alertType: "error",
           });
         }
       } else {
@@ -408,7 +414,7 @@ export default defineComponent({
                 console.error(`error occurred: ${error}`);
                 store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
                   msg: `error occurred: ${error}`,
-                  alertType: 'error'
+                  alertType: "error",
                 });
                 store.dispatch(ActionTypes.SET_LOADING, { loading: false });
                 return;
@@ -416,10 +422,10 @@ export default defineComponent({
 
               if (status.status.isFinalized) {
                 const hashResult = contributeTransaction.hash.toHex();
-                console.log('hashResult', hashResult);
+                console.log("hashResult", hashResult);
                 store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
                   msg: `Staking Complete...!`,
-                  alertType: 'success'
+                  alertType: "success",
                 });
                 store.dispatch(ActionTypes.SET_LOADING, { loading: false });
                 resultHash.value = hashResult;
@@ -433,7 +439,7 @@ export default defineComponent({
           console.error(err);
           store.dispatch(ActionTypes.SHOW_ALERT_MSG, {
             msg: `error occurred: ${err}`,
-            alertType: 'error'
+            alertType: "error",
           });
         }
       }
@@ -450,9 +456,9 @@ export default defineComponent({
       allAccountNames,
       modalAccount,
       modalDisclaimer,
-      onShowModalDisclaimer
+      onShowModalDisclaimer,
     };
-  }
+  },
 });
 </script>
 
@@ -474,14 +480,14 @@ button[disabled] {
   background-size: 550px;
   background-repeat: no-repeat;
   background-position: 0 bottom;
-  background-image: url('~@/assets/img/stake-dot-left.png');
+  background-image: url("~@/assets/img/stake-dot-left.png");
 }
 
 .background-right {
   background-size: 300px;
   background-repeat: no-repeat;
   background-position: right top;
-  background-image: url('~@/assets/img/stake-dot-right.png');
+  background-image: url("~@/assets/img/stake-dot-right.png");
 }
 
 .form-container {
