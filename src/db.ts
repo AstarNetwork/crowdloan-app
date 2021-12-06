923022869464a48e435d0bed853221b14c9e30c5
Will Park <korea651@hanmail.net>
2021-12-06 18:12:04 +0900

    get static json instead of fb

--- src/db.ts
+++ src/db.ts
@@ -16,7 +16,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
 const fetchEvent = async () => {
-  const docRef = doc(db, 'board-list/data');
+  const docRef = doc(db, 'board-list-s/data');
   const docData = await getDoc(docRef);
   // console.log('snap', docData.data())
   return docData.data();
