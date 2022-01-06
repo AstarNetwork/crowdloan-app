4d2a97f601d35230b5ef787cb89cc31b99fe40f7
Will Park <korea651@hanmail.net>
2022-01-06 22:55:13 +0900

    change reward ratio

--- src/config/crowdloan.ts
+++ src/config/crowdloan.ts
@@ -2,6 +2,6 @@
 export const PARA_ID = 2006;
 export const MINIMUM_STAKING_AMOUNT = 5;
 export const DEFAULT_REWARD_AMOUNT = 150;
-export const REWARD_RATIO = 30;
+export const REWARD_RATIO = 101;
 export const MIN_BALANCE = 1.1;
 export const UNIT = 10;
