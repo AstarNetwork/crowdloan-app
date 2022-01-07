9007f4d789410bbae3f4369a79445bca1e39b9ac
Will Park <korea651@hanmail.net>
2022-01-07 22:25:31 +0900

    Put out unused api-provider

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 import Main from '@/views/Main.vue';
 import ExternalHeader from '@/views/ExternalHeader.vue';
-import LeaderBoard from '@/views/LeaderBoard.vue';
+// import LeaderBoard from '@/views/LeaderBoard.vue';
 import EarlyAdopter from '@/views/EarlyAdopter.vue';
 
 const routes: RouteRecordRaw[] = [
@@ -11,11 +11,11 @@ const routes: RouteRecordRaw[] = [
     name: 'Main',
     component: Main
   },
-  {
-    path: '/leader-board',
-    name: 'LeaderBoard',
-    component: LeaderBoard
-  },
+  // {
+  //   path: '/leader-board',
+  //   name: 'LeaderBoard',
+  //   component: LeaderBoard
+  // },
   {
     path: '/early-adopter',
     name: 'EarlyAdopter',
